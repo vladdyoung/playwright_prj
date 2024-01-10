c5dda241c78654b96fcc50649e5393e32c145303
DemyanenkoVYu <DemyanenkoVYu@akbarsdigital.ru>
2024-01-10 14:48:45 +0300

    Настраивает работу с Vault

--- tests/oscar_sandbox/test_main_page.py
+++ tests/oscar_sandbox/test_main_page.py
@@ -8,16 +8,16 @@ class TestMainPage:
 
     @logger.catch(reraise=True)
     @allure.title('Проверка смены языка')
-    def test_change_language(self, page, base_url):
-        page = MainPage(page=page, url=base_url)
+    def test_change_language(self, page, session, base_url):
+        page = MainPage(page=page, session=session, url=base_url)
         page.open()
         page.change_language()
         page.should_be_changed_language()
 
     @logger.catch(reraise=True)
     @allure.title('Проверка работы поля поиска продукта')
-    def test_search_field(self, page, base_url):
-        page = MainPage(page=page, url=base_url)
+    def test_search_field(self, page, session, base_url):
+        page = MainPage(page=page, session=session, url=base_url)
         page.open()
         page.fill_search_field_by_character()
         page.should_be_work_search_fill()
